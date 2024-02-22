545d015f60bef4b4346bfa7f5f9ed485925be451
Yitzhak Cohen <30642253+cohenyitz@users.noreply.github.com>
2024-02-22 03:01:56 -0500

    Commit from TeleportHQ

---
+++ tailwind.config.js
@@ -0,0 +1,70 @@
+module.exports = {
+  "theme": {
+    "extend": {
+      "colors": {
+        "Primary": {
+          "100": "#003EB3",
+          "300": "#0074F0",
+          "500": "#14A9FF",
+          "700": "#85DCFF"
+        },
+        "Gray": {
+          "500": "#595959",
+          "700": "#999999",
+          "900": "#D9D9D9",
+          "Black": "#000000",
+          "White": "#FFFFFF"
+        },
+        "Success": {
+          "300": "#199033",
+          "500": "#32A94C",
+          "700": "#4CC366"
+        },
+        "Danger": {
+          "300": "#A22020",
+          "500": "#BF2626",
+          "700": "#E14747"
+        },
+        "custom": {
+          "primary1": "#147487",
+          "primary2": "#0A3A44",
+          "secondary1": "#A9D5DD",
+          "accent1": "#C9E8FF",
+          "neutral-dark": "#222222",
+          "neutral-light": "#F5F4F4"
+        },
+        "foreground": "#000000",
+        "background": "#FFFFFF"
+      },
+      "spacing": {
+        "HalfUnit": "8px",
+        "Unit": "16px",
+        "OneAndHalfUnits": "24px",
+        "TwoUnits": "32px",
+        "ThreeUnits": "48px",
+        "FourUnits": "64px",
+        "FiveUnits": "80px",
+        "SixUnits": "96px"
+      },
+      "borderRadius": {
+        "Radius2": "2px",
+        "Radius4": "4px",
+        "Radius8": "8px",
+        "Round": "50%"
+      },
+      "scale": {
+        "XSmall": "16px",
+        "Small": "48px",
+        "Medium": "96px",
+        "Large": "144px",
+        "XLarge": "192px",
+        "XXLarge": "288px",
+        "MaxWidth": "1400px"
+      }
+    }
+  },
+  "plugins": [],
+  "content": [
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ]
+}
